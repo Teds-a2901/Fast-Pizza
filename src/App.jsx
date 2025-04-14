68a8b41fb58745af694e4b3bd065b7b755cf3884
Teds <marktedra29@gmail.com>
2025-04-14 21:43:44 +0800

    ADD API MENU

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu, {loader as menuLoader} from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [{
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/menu",
+      element: <Menu /> ,
+      loader: menuLoader, 
+    },
+    {
+      path: "/cart",
+      element: <Cart />,
+    },
+    { path: "/create/new", element: <CreateOrder /> },
+    { path: "/order/:oderId", element: <Order />}]
+  },
+  
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+export default App;
