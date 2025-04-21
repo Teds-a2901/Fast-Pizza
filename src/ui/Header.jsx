342a820183c70e937a27cbebb2cc709b5ff247e3
Teds <marktedra29@gmail.com>
2025-04-21 15:43:28 +0800

    do a thing
    install redux
    create store file to store the usersname
    createslice for users to get update the usersname state

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import SearchOrder from '../features/order/SearchOrder'
+import Username from '../features/user/Username'
 
 function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6 font-pizza">
         <Link to = "/" className="tracking-widest">Fast React Pizza</Link>
         <SearchOrder/>
-        <p>Teds Zed</p>
+        <Username/>
     </header>
   )
 }
